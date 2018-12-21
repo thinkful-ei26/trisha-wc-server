f1e9b0d4c6afc2c32346cce4582ec44e2572c76e
kronicle114 <trisha.aguinaldo@gmail.com>
2018-12-21 13:03:57 -0500

    basic server test all passing

--- server.js
+++ server.js
@@ -1,8 +1,61 @@
 'use strict';
 
 const express = require('express');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+
+const { PORT, DATABASE_URL } = require('./config');
+
+// Create an Express application
 const app = express();
 
-//pulling from server/public/index.html this is a test
+// Log all requests. Skip logging during
+app.use(morgan(process.env.NODE_ENV === 'development' ? 'dev' : 'common', {
+  skip: () => process.env.NODE_ENV === 'test'
+}));
+
+// Create a static webserver 
+//this is a test for the server, will replace with react client side)
 app.use(express.static('public'));
-app.listen(process.env.PORT || 8080);
+
+// Parse request body
+app.use(express.json());
+
+// Custom 404 Not Found route handler
+app.use((req, res, next) => {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// Error first handler
+app.use((err, req, res, next) => {
+  if (err.status) {
+    const errBody = Object.assign({}, err, { message: err.message });
+    res.status(err.status).json(errBody);
+  } else {
+    res.status(500).json({ message: 'Internal Server Error' });
+    console.log(err.name === 'FakeError' ? '' : err);
+  }
+});
+
+// Listen for incoming connections
+if (require.main === module) {
+  // Connect to DB and Listen for incoming connections
+  mongoose.connect(DATABASE_URL)
+    .then(instance => {
+      const conn = instance.connections[0];
+      console.info(`Connected to: mongodb://${conn.host}:${conn.port}/${conn.name}`);
+    })
+    .catch(err => {
+      console.error(err);
+    });
+
+  app.listen(PORT, function () {
+    console.info(`Server listening on ${this.address().port}`);
+  }).on('error', err => {
+    console.error(err);
+  });
+}
+
+module.exports = app; // Export for testing
--- test/setup.test.js
+++ test/setup.test.js
@@ -3,9 +3,7 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 
-const {TEST_DATABASE_URL} = require('../config');
-const {dbConnect, dbDisconnect} = require('../db-mongoose');
-// const {dbConnect, dbDisconnect} = require('../db-knex');
+const app = require('../server');
 
 // Set NODE_ENV to `test` to disable http layer logs
 // You can do this in the command line, but this is cross-platform
@@ -17,16 +15,51 @@ process.stdout.write('\x1Bc\n');
 const expect = chai.expect;
 chai.use(chaiHttp);
 
-before(() => {
-  return dbConnect(TEST_DATABASE_URL);
-});
+describe('Reality Check', () => {
 
-after(() => {
-  return dbDisconnect();
-});
-
-describe('Mocha and Chai', () => {
-  it('server setup working', () => {
+  it('true should be true', () => {
     expect(true).to.be.true;
+  });
+
+  it('2 + 2 should equal 4', () => {
+    expect(2 + 2).to.equal(4);
+  });
+
+});
+
+describe('Environment', () => {
+
+  it('NODE_ENV should be "test"', () => {
+    expect(process.env.NODE_ENV).to.equal('test');
+  });
+
+});
+
+describe('Basic Express setup', () => {
+
+  describe('Express static', () => {
+
+    it('GET request "/" should return the index page', () => {
+      return chai.request(app)
+        .get('/')
+        .then(function (res) {
+          expect(res).to.exist;
+          expect(res).to.have.status(200);
+          expect(res).to.be.html;
+        });
+    });
+
+  });
+
+  describe('404 handler', () => {
+
+    it('should respond with 404 when given a bad path', () => {
+      return chai.request(app)
+        .get('/bad/path')
+        .then(res => {
+          expect(res).to.have.status(404);
+        });
+    });
+
   });
 });
