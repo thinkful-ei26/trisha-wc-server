98e3c49eb870e7a7f9d51c0e8bcfced809b823e4
kronicle114 <trisha.aguinaldo@gmail.com>
2018-12-26 17:31:28 -0500

    seeded db

--- db/data.js
+++ db/data.js
@@ -9,7 +9,8 @@ const recipes = [
     imgUrl: 'https://sweettootsco.files.wordpress.com/2018/12/zucchini-meatballs.png',
     prep: '30',
     cook: '45',
-    directions: 'Heat the oven to 375°F. Spray a large rimmed baking sheet with cooking spray. Place the olive oil in a large nonstick skillet over medium heat. When hot add the garlic and saute until golden, about 30 seconds. Add the zucchini, season with salt and pepper and cook on high heat until all the water evaporates from the skillet, 5 to 7 minutes. Transfer to a colander to get rid of any excess water, then to a large bowl along with the bread crumbs, beaten egg, Romano cheese and chopped basil. Form the zucchini mixture into 16 balls about 1 oz each, rolling tightly and transfer to the prepared baking sheet. Spray the top and bake until firm and browned, about 20 to 25 minutes. Heat the sauce in a large deep skillet to warm the sauce, about 4 to 5 minutes. Add the meatballs to the sauce and simmer for 5 minutes. Garnish the meatballs with fresh basil leaves and serve with ricotta or grated cheese if desired.'
+    directions: 'Heat the oven to 375°F. Spray a large rimmed baking sheet with cooking spray. Place the olive oil in a large nonstick skillet over medium heat. When hot add the garlic and saute until golden, about 30 seconds. Add the zucchini, season with salt and pepper and cook on high heat until all the water evaporates from the skillet, 5 to 7 minutes. Transfer to a colander to get rid of any excess water, then to a large bowl along with the bread crumbs, beaten egg, Romano cheese and chopped basil. Form the zucchini mixture into 16 balls about 1 oz each, rolling tightly and transfer to the prepared baking sheet. Spray the top and bake until firm and browned, about 20 to 25 minutes. Heat the sauce in a large deep skillet to warm the sauce, about 4 to 5 minutes. Add the meatballs to the sauce and simmer for 5 minutes. Garnish the meatballs with fresh basil leaves and serve with ricotta or grated cheese if desired.',
+    condensed: true
   },
   {
     _id: '111111111111111111111102',
@@ -19,17 +20,30 @@ const recipes = [
     imgUrl: 'https://sweettootsco.files.wordpress.com/2018/12/ddukbokki.png',
     prep: '30',
     cook: '45',
-    directions: 'Heat the oven to 375°F. Spray a large rimmed baking sheet with cooking spray. Place the olive oil in a large nonstick skillet over medium heat. When hot add the garlic and saute until golden, about 30 seconds. Add the zucchini, season with salt and pepper and cook on high heat until all the water evaporates from the skillet, 5 to 7 minutes. Transfer to a colander to get rid of any excess water, then to a large bowl along with the bread crumbs, beaten egg, Romano cheese and chopped basil. Form the zucchini mixture into 16 balls about 1 oz each, rolling tightly and transfer to the prepared baking sheet. Spray the top and bake until firm and browned, about 20 to 25 minutes. Heat the sauce in a large deep skillet to warm the sauce, about 4 to 5 minutes. Add the meatballs to the sauce and simmer for 5 minutes. Garnish the meatballs with fresh basil leaves and serve with ricotta or grated cheese if desired.'
+    directions: 'Heat the oven to 375°F. Spray a large rimmed baking sheet with cooking spray. Place the olive oil in a large nonstick skillet over medium heat. When hot add the garlic and saute until golden, about 30 seconds. Add the zucchini, season with salt and pepper and cook on high heat until all the water evaporates from the skillet, 5 to 7 minutes. Transfer to a colander to get rid of any excess water, then to a large bowl along with the bread crumbs, beaten egg, Romano cheese and chopped basil. Form the zucchini mixture into 16 balls about 1 oz each, rolling tightly and transfer to the prepared baking sheet. Spray the top and bake until firm and browned, about 20 to 25 minutes. Heat the sauce in a large deep skillet to warm the sauce, about 4 to 5 minutes. Add the meatballs to the sauce and simmer for 5 minutes. Garnish the meatballs with fresh basil leaves and serve with ricotta or grated cheese if desired.',
+    condensed: true
   },
   {
     _id: '111111111111111111111103',
     title: 'bacon scallop vinegrette',
-    desc: 'recipe for banana bread',
+    desc: 'Delicious and easy bacon scallop',
     ing: ['1/2 lb of beef', 'salt', 'pepper', 'zucchini' ],
-    imgUrl: 'bacon-scallop-vinegrette',
+    imgUrl: 'https://sweettootsco.files.wordpress.com/2018/12/bacon-scallop-vinegrette.png',
     prep: '30',
     cook: '45',
-    directions: 'Heat the oven to 375°F. Spray a large rimmed baking sheet with cooking spray. Place the olive oil in a large nonstick skillet over medium heat. When hot add the garlic and saute until golden, about 30 seconds. Add the zucchini, season with salt and pepper and cook on high heat until all the water evaporates from the skillet, 5 to 7 minutes. Transfer to a colander to get rid of any excess water, then to a large bowl along with the bread crumbs, beaten egg, Romano cheese and chopped basil. Form the zucchini mixture into 16 balls about 1 oz each, rolling tightly and transfer to the prepared baking sheet. Spray the top and bake until firm and browned, about 20 to 25 minutes. Heat the sauce in a large deep skillet to warm the sauce, about 4 to 5 minutes. Add the meatballs to the sauce and simmer for 5 minutes. Garnish the meatballs with fresh basil leaves and serve with ricotta or grated cheese if desired.'
+    directions: 'Heat the oven to 375°F. Spray a large rimmed baking sheet with cooking spray. Place the olive oil in a large nonstick skillet over medium heat. When hot add the garlic and saute until golden, about 30 seconds. Add the zucchini, season with salt and pepper and cook on high heat until all the water evaporates from the skillet, 5 to 7 minutes. Transfer to a colander to get rid of any excess water, then to a large bowl along with the bread crumbs, beaten egg, Romano cheese and chopped basil. Form the zucchini mixture into 16 balls about 1 oz each, rolling tightly and transfer to the prepared baking sheet. Spray the top and bake until firm and browned, about 20 to 25 minutes. Heat the sauce in a large deep skillet to warm the sauce, about 4 to 5 minutes. Add the meatballs to the sauce and simmer for 5 minutes. Garnish the meatballs with fresh basil leaves and serve with ricotta or grated cheese if desired.',
+    condensed: true
+  },
+  {
+    _id: '111111111111111111111104',
+    title: 'Katsu Curry',
+    desc: 'Japanese pork cutlet with curry noodles',
+    ing: ['1/2 lb of beef', 'salt', 'pepper', 'zucchini' ],
+    imgUrl: 'https://sweettootsco.files.wordpress.com/2018/12/katsu-curry.png',
+    prep: '30',
+    cook: '45',
+    directions: 'Heat the oven to 375°F. Spray a large rimmed baking sheet with cooking spray. Place the olive oil in a large nonstick skillet over medium heat. When hot add the garlic and saute until golden, about 30 seconds. Add the zucchini, season with salt and pepper and cook on high heat until all the water evaporates from the skillet, 5 to 7 minutes. Transfer to a colander to get rid of any excess water, then to a large bowl along with the bread crumbs, beaten egg, Romano cheese and chopped basil. Form the zucchini mixture into 16 balls about 1 oz each, rolling tightly and transfer to the prepared baking sheet. Spray the top and bake until firm and browned, about 20 to 25 minutes. Heat the sauce in a large deep skillet to warm the sauce, about 4 to 5 minutes. Add the meatballs to the sauce and simmer for 5 minutes. Garnish the meatballs with fresh basil leaves and serve with ricotta or grated cheese if desired.',
+    condensed: true
   },
 ];
 
